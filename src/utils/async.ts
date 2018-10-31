234b25dd253d15d15f90392d94fd1a78e2dcc742
Matus Zamborsky <zamborsky@gmail.com>
2018-10-31 14:12:10 +0100

    hide password

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -5,9 +5,28 @@ export function questionAsync(msg: string): Promise<string> {
     input: process.stdin,
     output: process.stdout
   });
+  const rlAny: any = rl;
+
+  const stdin = process.openStdin();
+  const onDataHandler = (char: string) => {
+    char = char + '';
+    switch (char) {
+      case '\n':
+      case '\r':
+      case '\u0004':
+        // Remove this handler
+        stdin.removeListener('data', onDataHandler);
+        break; // stdin.pause(); break;
+      default:
+        process.stdout.write('\x1B[2K\x1B[200D' + msg + Array(rlAny.line.length + 1).join('*'));
+        break;
+    }
+  };
+  process.stdin.on('data', onDataHandler);
 
   return new Promise((resolve) => {
-    rl.question(msg, (answer) => {
+    rl.question(msg, (answer: string) => {
+      rlAny.history = rlAny.history.slice(1);
       rl.close();
       resolve(answer);
     });

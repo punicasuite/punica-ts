4aa6608deb9ad41c52b91d8d76eb94d1e77e4073
Matus Zamborsky <zamborsky@gmail.com>
2018-11-02 15:27:45 +0100

    Asset management (#3)
    * init commit
    
    Signed-off-by: Matus Zamborsky <zamborsky@gmail.com>
    
    * add compile
    
    Signed-off-by: Matus Zamborsky <zamborsky@gmail.com>
    
    * removed unecessary dependencies, WIP deployer
    
    * WIP deployer
    
    * wallet decrypt
    
    * added Deployer
    
    * remove unecessary dependency
    
    * WIP wallet manager
    
    * wallet manager add and delete
    
    * fix git clone, add import account
    
    * fix punica-config.json
    
    * defaultNet support
    
    * add sc invoking
    
    * proper version
    
    * proper result handling for invoke
    
    * all function listing
    
    * password config loader
    
    * fixed relative compile
    
    * better source map support
    
    * print network
    
    * test scrypt
    
    * bumped versions
    
    * hide password
    
    * ignore tests
    
    * make projectDir absolute, add test template + exec
    
    * Create README.md
    
    * Update README.md
    
    * fix some description
    
    * add identity methods
    
    * fix identities
    
    * WIP: asset management
    
    * add balanceOf and unboundOng support
    
    * add transfer and withdrawOng support

--- src/asset/assets.ts
+++ src/asset/assets.ts
@@ -1,21 +1,99 @@
 import BigNumber from 'bignumber.js';
 import { Address } from 'ontology-ts-crypto';
-import { RpcClient } from 'ontology-ts-test';
-import { loadNetwork } from '../config/configLoader';
+import { initClient, RpcClient, transfer, withdrawOng } from 'ontology-ts-test';
+import { loadAccount, loadNetwork, loadWallet } from '../config/configLoader';
 import { otherError } from '../exception/punicaException';
+import { inputExistingPassword } from '../wallet/walletCli';
 
 export type Asset = 'ont' | 'ong';
 
+// tslint:disable:no-console
+
 export class Assets {
   async transfer(
-    asset: Asset,
+    projectDir: string,
+    asset: Asset = 'ont',
     sender: string,
     to: string,
+    walletFileName: string | undefined,
     amount: string,
     gasPrice: string,
     gasLimit: string,
     networkKey?: string
-  ) {}
+  ) {
+    asset = asset.toLowerCase() as Asset;
+
+    if (asset !== 'ont' && asset !== 'ong') {
+      throw otherError('asset should be ont or ong');
+    }
+
+    const [wallet] = loadWallet(projectDir, walletFileName);
+
+    const account = loadAccount(wallet, sender);
+    const password = await inputExistingPassword('Please input sender account password: ');
+
+    const rpcAddress = loadNetwork(projectDir, networkKey);
+    const client = initClient({ rpcAddress });
+
+    const response = await transfer({
+      client,
+      account,
+      password,
+      sender: account.address,
+      to: Address.fromBase58(to),
+      amount,
+      asset,
+      wait: false,
+      gasLimit,
+      gasPrice
+    });
+
+    const result = response.result;
+    if (result !== '') {
+      console.log(`Transaction: ${result}`);
+    } else {
+      console.warn(`Transfer failed. Error: ${result}.`);
+    }
+  }
+
+  async withdrawOng(
+    projectDir: string,
+    claimer: string,
+    to: string,
+    walletFileName: string | undefined,
+    amount: string,
+    gasPrice: string,
+    gasLimit: string,
+    networkKey?: string
+  ) {
+    const [wallet] = loadWallet(projectDir, walletFileName);
+
+    const account = loadAccount(wallet, claimer);
+    const password = await inputExistingPassword('Please input claimer account password: ');
+
+    const rpcAddress = loadNetwork(projectDir, networkKey);
+    const client = initClient({ rpcAddress });
+
+    const response = await withdrawOng({
+      client,
+      account,
+      asset: 'ong',
+      password,
+      sender: account.address,
+      to: Address.fromBase58(to),
+      amount,
+      wait: false,
+      gasLimit,
+      gasPrice
+    });
+
+    const result = response.result;
+    if (result !== '') {
+      console.log(`Transaction: ${result}`);
+    } else {
+      console.warn(`Withdraw ONG failed. Error: ${result}.`);
+    }
+  }
 
   async balanceOf(projectDir: string, asset: Asset = 'ong', address: string, networkKey?: string) {
     asset = asset.toLowerCase() as Asset;
@@ -39,15 +117,6 @@ export class Assets {
       throw otherError(response.desc);
     }
   }
-
-  async withdrawOng(
-    claimer: string,
-    to: string,
-    amount: string,
-    gasPrice: string,
-    gasLimit: string,
-    networkKey?: string
-  ) {}
 
   async unboundOng(projectDir: string, address: string, networkKey?: string) {
     const rpcAddress: string = loadNetwork(projectDir, networkKey);
